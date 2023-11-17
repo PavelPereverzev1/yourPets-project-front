30d8b14abbae8894a49262a4585c9992ab9bf679
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-11-17 09:42:45 +0200

    bug fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
-import { FilterByAge } from './components/FilterByAge';
-import { FilterByGender } from './components/FilterByGender';
+// import React, { useState } from 'react';
+// import { FilterByAge } from './components/FilterByAge';
+// import { FilterByGender } from './components/FilterByGender';
 
-export const Filter = () => {
-  const [display, setDisplay] = useState(false);
-  const [filter, setFilter] = useState({
-    upToOneYear: false,
-    upToTwoYears: false,
-    fromTwoYears: false,
-    female: false,
-    male: false,
-  });
-  const handleFilterChange = filter => {
-    setDisplay(prevState => {
-      return { ...prevState, ...filter };
-    });
-  };
-  return (
-    <>
-      <button onClick={() => setDisplay(!display)}>Filter</button>
-      {display && (
-        <div>
-          <FilterByAge onFilterChange={handleFilterChange} />
-          <FilterByGender />
-        </div>
-      )}
-    </>
-  );
-};
+// export const Filter = () => {
+//   const [display, setDisplay] = useState(false);
+//   const [filter, setFilter] = useState({
+//     upToOneYear: false,
+//     upToTwoYears: false,
+//     fromTwoYears: false,
+//     female: false,
+//     male: false,
+//   });
+//   const handleFilterChange = filter => {
+//     setDisplay(prevState => {
+//       return { ...prevState, ...filter };
+//     });
+//   };
+//   return (
+//     <>
+//       <button onClick={() => setDisplay(!display)}>Filter</button>
+//       {display && (
+//         <div>
+//           <FilterByAge onFilterChange={handleFilterChange} />
+//           <FilterByGender />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
