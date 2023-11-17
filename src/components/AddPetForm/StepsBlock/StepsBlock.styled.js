e4a5eeabd82f7b73713999bf0b335075c7e2de00
Tamira2804 <tamira2804@gmail.com>
2023-11-17 20:44:30 +0200

    added StepsBlock

---
+++ src/components/AddPetForm/StepsBlock/StepsBlock.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const StepsBlockWrapper = styled.div`
+  display: flex;
+  gap: 12px;
+  margin-bottom: 24px;
+`;
+
+export const StepBlock = styled.div`
+  width: 100%;
+`;
+
+export const StepName = styled.p`
+  color: #888;
+  text-align: start;
+  font-family: Manrope;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin-bottom: 12px;
+`;
+export const StepLine = styled.div`
+  width: 100%;
+  height: 8px;
+  border-radius: 8px;
+  background-color: #cce4fb;
+`;
