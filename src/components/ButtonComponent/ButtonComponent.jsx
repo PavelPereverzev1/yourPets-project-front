345e1d4093d75751ac8a85c9782aceae64746e4d
alex12121983 <alex12121983@gmail.com>
2023-11-18 19:41:47 +0100

    ButtonComponent update
    The props {name} changed by {child}

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Button } from './ButtonComponent.styled';
 
-export default function ButtonComponent({ name, width }) {
+export default function ButtonComponent({ children, width }) {
   return (
     <>
       <Button type="button" width={width}>
-        {name}
+        {children}
       </Button>
     </>
   );
