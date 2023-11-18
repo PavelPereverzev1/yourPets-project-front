aea422c1aafba230e63560f991df15d02ec4c468
yurii-corssa <yurii.corssa@gmail.com>
2023-11-18 18:27:22 +0100

    fix

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from 'components/Header/Header';
 import { Container } from './SharedLayout.styled';
-import UserPage from 'pages/UserPage';
 
 const SharedLayout = () => {
   return (
@@ -11,7 +10,6 @@ const SharedLayout = () => {
         <Header />
         <Outlet />
       </Container>
-      <UserPage />
     </>
   );
 };
