e28ea4335047de99cf9f636cd1797e81568bed7c
Vova <vovazbuchkin9@gmail.com>
2023-11-19 13:13:25 +0200

    fix

--- src/components/NoticeCategoriesNav/NoticeCategoryItem/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoriesNav/NoticeCategoryItem/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,5 +1,4 @@
-
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ImageBlock,
   Image,
@@ -27,103 +26,88 @@ import {
   FemaleIcon,
 } from './SvgIcons.jsx';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import data from "./data.json"
+import data from './data.json';
 
-
-
-const NoticeCard = ({ }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
+const NoticeCard = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [dataf, setDataf] = useState([]);
 
   useEffect(() => {
+    setDataf(data.map(item => ({ ...item, isFavorite: false })));
    
   }, []);
 
-  const handleAddToFavorite = () => {
-
+  const handleAddToFavorite = (id) => {
     if(!isLoggedIn){
-      setIsFavorite(isFavorite)
-      Notify.failure("Please, sign in")
-    }
-    else{
-      setIsFavorite(!isFavorite);
-    }
-     if(isLoggedIn){
       setIsLoggedIn(isLoggedIn)
     }
+    if (isLoggedIn) {
+      Notify.failure('Please, sign in');
+    } else {
       
-    
+      const updatedData = dataf.map((item) =>
+        item.id === id ? { ...item, isFavorite: !item.isFavorite } : item
+      );
+      setDataf(updatedData);
+    }
   };
-  const handleLearnMore = () => {
-  
-  
-  }
 
+  const handleLearnMore = () => {
+   
+  };
 
   return (
     <>
-    
-
-          <Container>
-        {data.map(data => {
-          return(
-          <Item key="id">
-          <ImageBlock>
-            <InGoodHands>In good hands</InGoodHands>
-            <Favorite
-           onClick={() => handleAddToFavorite(data.id)}
-            >
-              <FavoriteIcon isFavorite={isFavorite}></FavoriteIcon>
-            </Favorite>
-            {isLoggedIn && (
-          <Favorite
-            onClick={handleAddToFavorite}
-            
-            style={{
-              backgroundColor: isFavorite ? 'yellow' : 'yellow',
-            }}
-          >
-            <FavoriteIcon isFavorite={isFavorite}></FavoriteIcon>
-            {isFavorite ? "Видалити з обраних" : "Додати до обраних"}
-          </Favorite>
-        )}
-            <InfoLocation>
-              <LocationIcon></LocationIcon>
-              <InfoText>{data.location}</InfoText>
-            </InfoLocation>
-            <InfoAge>
-              <AgeIcon></AgeIcon>
-              <InfoText>{data.age} year</InfoText>
-            </InfoAge>
-            <InfoPol>
-            {data.sex === 'Male' ? (
-                <MaleIcon></MaleIcon>
-              ) : data.sex === 'Female' ? (
-                <FemaleIcon></FemaleIcon>
-              ): null}
-              <InfoText>{data.sex}</InfoText>
-            </InfoPol>
-
-            <Image src={myImg}></Image>
-          </ImageBlock>
-          <TextDiv>
-            <Text>Сute cat looking for a home</Text>
-          </TextDiv>
-          <LearnMoreDiv>
-            <LearnMore onClick={handleLearnMore}>
-              <TextMore>Learn more</TextMore>
-              <PetIcon></PetIcon>
-            </LearnMore>
-          </LearnMoreDiv>
-        </Item>
-          )
-        })}
-          
-        </Container>
-      
+      <Container>
+        {dataf.map((item) => (
+          <Item key={item.id}>
+            <ImageBlock>
+              <InGoodHands>In good hands</InGoodHands>
+              <Favorite onClick={() => handleAddToFavorite(item.id)}>
+                <FavoriteIcon isFavorite={item.isFavorite}></FavoriteIcon>
+              </Favorite>
+              {isLoggedIn && (
+                <Favorite
+                  onClick={() => handleAddToFavorite(item.id)}
+                  style={{
+                    backgroundColor: item.isFavorite ? 'yellow' : 'yellow',
+                  }}
+                >
+                  <FavoriteIcon isFavorite={item.isFavorite}></FavoriteIcon>
+                  {item.isFavorite ? 'Видалити з обраних' : 'Додати до обраних'}
+                </Favorite>
+              )}
+              <InfoLocation>
+                <LocationIcon></LocationIcon>
+                <InfoText>{item.location}</InfoText>
+              </InfoLocation>
+              <InfoAge>
+                <AgeIcon></AgeIcon>
+                <InfoText>{item.age} year</InfoText>
+              </InfoAge>
+              <InfoPol>
+                {item.sex === 'Male' ? (
+                  <MaleIcon></MaleIcon>
+                ) : item.sex === 'Female' ? (
+                  <FemaleIcon></FemaleIcon>
+                ) : null}
+                <InfoText>{item.sex}</InfoText>
+              </InfoPol>
+              <Image src={myImg}></Image>
+            </ImageBlock>
+            <TextDiv>
+              <Text>Сute cat looking for a home</Text>
+            </TextDiv>
+            <LearnMoreDiv>
+              <LearnMore onClick={handleLearnMore}>
+                <TextMore>Learn more</TextMore>
+                <PetIcon></PetIcon>
+              </LearnMore>
+            </LearnMoreDiv>
+          </Item>
+        ))}
+      </Container>
     </>
-     
   );
 };
 
