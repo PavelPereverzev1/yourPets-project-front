72c9f8efc06eb54a6c6477c404f578fd79878b43
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-11-20 14:27:35 +0200

    add-icons

--- src/components/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import {Queryform} from './NoticesSearch.styled'
+import React from 'react';
+import {
+  Label,
+  Queryform,
+  ButtonSearch,
+  SearchIcon,
+  ButtonCancel,
+  DelateIcon,
+  ButtonWrapper,
+} from './NoticesSearch.styled';
+import sprite from '../../images/icons/sprite.svg';
 
 function NoticesSearch() {
   return (
-    <>
-    <Queryform placeholder='Search' />
-    </>
-  )
+    <Label>
+      <Queryform placeholder="Search" />
+      <ButtonWrapper>
+        <ButtonSearch>
+          <SearchIcon>
+            <use href={`${sprite}#icon-search`}></use>
+          </SearchIcon>
+        </ButtonSearch>
+        <ButtonCancel>
+          <DelateIcon>
+            <use href={`${sprite}#icon-cross-small`}></use>
+          </DelateIcon>
+        </ButtonCancel>
+      </ButtonWrapper>
+    </Label>
+  );
 }
 
-export default NoticesSearch
+export default NoticesSearch;
--- src/components/NoticesSearch/NoticesSearch.styled.js
+++ src/components/NoticesSearch/NoticesSearch.styled.js
@@ -1,31 +1,73 @@
 import styled from 'styled-components';
 
+export const Label = styled.label`
+  position: relative;
+`;
+
 export const Queryform = styled.input`
-width: 280px;
-height: 44px;
-border-radius: 24px;
-border: none;
-margin-top: 24px;
-margin-bottom: 20px;
-display: flex;
-justify-content: center;
-box-shadow: 3px 8px 14px 0 #88C6FD30;
-outline: none;
-padding-left: 20px;
-padding-right: 10px;
+  width: 280px;
+  height: 44px;
+  border-radius: 24px;
+  border: none;
+  margin-top: 24px;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  box-shadow: 3px 8px 14px 0 #88c6fd30;
+  outline: none;
+  padding-left: 20px;
+  padding-right: 10px;
+  font-family: Inter, sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.21;
+  letter-spacing: 0.04em;
+  color: #888888;
 
-&:hover,
-&:focus {
-    box-shadow: 7px 13px 14px 0 #74B1E8;
-}
+  &:hover,
+  &:focus {
+    box-shadow: 7px 13px 14px 0 #74b1e8;
+  }
 
-&::placeholder{
+  &::placeholder {
     font-family: Inter, sans-serif;
     font-weight: 400;
     font-size: 14px;
     line-height: 1.21;
     letter-spacing: 0.04em;
     color: #888888;
-}
+  }
+`;
 
-`
+export const ButtonWrapper = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 244px;
+  display: flex;
+`;
+
+export const ButtonSearch = styled.button`
+  background-color: inherit;
+  border: none;
+  width: 24px;
+  height: 24px;
+`;
+
+export const SearchIcon = styled.svg`
+  width: 24px;
+  height: 24px;
+`;
+
+export const ButtonCancel = styled.button`
+  background-color: inherit;
+  border: none;
+  width: 24px;
+  height: 24px;
+  margin-left: 10px;
+`;
+
+export const DelateIcon = styled.svg`
+  width: 24px;
+  height: 24px;
+  stroke: #ffc107;
+`;
