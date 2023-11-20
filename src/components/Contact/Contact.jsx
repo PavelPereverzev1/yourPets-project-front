1422e9ff1debf5b3fffb5bd51835a5459b4c8370
alex12121983 <alex12121983@gmail.com>
2023-11-20 23:42:59 +0100

    Update  Contact component
    Deleted unuse styled component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { ContactButton, ContactText } from './Contact.styled';
+import { ContactButton } from './Contact.styled';
 
 const Contact = ({ categoryItem }) => {
   return (
