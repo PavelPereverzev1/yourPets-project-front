9a2904858dc83e2a6449f9fc21b9f671aabf5037
PavelPereverzev1 <123000392+PavelPereverzev1@users.noreply.github.com>
2023-11-22 15:43:28 +0000

    Deploying to gh-pages from @ PavelPereverzev1/yourPets-project-front@9ae29c2688bd66f31bc9c7bdf9ecc32483e108c6 🚀

--- static/js/93.16e2f236.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[93],{3093:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184),u=function(){return(0,r.jsx)("div",{children:"Not Found"})}}}]);
-//# sourceMappingURL=93.16e2f236.chunk.js.map
