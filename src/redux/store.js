59893cd22f652527c0419a17f711a320af9cff61
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-11-27 17:26:16 +0200

    delete-comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,7 +13,6 @@ import storage from 'redux-persist/lib/storage';
 import { authReducer } from './AuthSlice';
 import { noticesPersistReducer } from './notices/noticesSlices';
 import { friendsReducer } from './OurFriends/OurFriendsSlice';
-// import { queryReducer } from './notices/noticesQuerySlice';
 import {NewsReducer} from './news/newsSlice'
 
 const authPersistConfig = {
@@ -27,7 +26,6 @@ export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
     notices: noticesPersistReducer,
-    // query: queryReducer,
     news: NewsReducer,
     friends: friendsReducer,
   },
