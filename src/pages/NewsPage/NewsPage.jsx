26d5e007508131672e5e14456809a1a022d5bc24
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-11-29 21:29:08 +0200

    add-pagination-fix-issues

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,30 +1,14 @@
-import TitleComponent from "components/TitleComponent/TitleComponent";
-import NoticesSearch from "components/NoticesSearch/NoticesSearch";
-import NewsList from "components/NewsList/NewsList";
-import Pagination from "components/Pagination/Pagination";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchNews } from "redux/news/news-thunk";
+import TitleComponent from 'components/TitleComponent/TitleComponent';
+import NoticesSearch from 'components/NoticesSearch/NoticesSearch';
+import NewsList from 'components/NewsList/NewsList';
 
 const NewsPage = () => {
-  const dispatch = useDispatch();
-  const currentPage = useSelector(state => state.currentPage);
-  const totalPages = useSelector(state => state.totalPages);
-
-  const handlePageChange = page => {
-    dispatch(fetchNews({ page }));
-  };
-
   return (
     <>
-   <TitleComponent name="News"/>
-   <NoticesSearch />
-  <NewsList />
-  <Pagination 
-  currentPage={currentPage}
-  totalPages={totalPages}
-  onPageChange={handlePageChange}
-  />
-  </>
+      <TitleComponent name="News" />
+      <NoticesSearch />
+      <NewsList />
+    </>
   );
 };
 
